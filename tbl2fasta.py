13bb3270c89ac6ae6ac9b9fde8a8da5c0080ab77
Emanuel Langit <emanuel.langit@fastmail.fm>
2015-09-13 20:02:13 -0700

    tbl2fasta script done

---
+++ tbl2fasta.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+Script to convert a tab-delimitted file into a fasta file.
+The first field is the defline, and the second field is the sequence
+"""
+import sys
+
+filename = sys.argv[1]
+
+tblFile = open(filename)
+
+for line in tblFile:
+	revisedLine1 = line.rstrip("\n")
+	revisedLine2 = revisedLine1.replace("\t", "\n")
+	print(">" + revisedLine2) 
+
+tblFile.close();
