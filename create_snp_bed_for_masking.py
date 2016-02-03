d2fe950faff53f48512e17315097b008df02e5d8
Emanuel Langit <emanuel.langit@fastmail.fm>
2016-02-02 19:31:29 -0800

    Got argparse working

--- create_snp_bed_for_masking.py
+++ create_snp_bed_for_masking.py
@@ -11,5 +11,23 @@ parser.add_argument('inputFile', metavar='arg1', help = 'file downloaded from UC
 parser.add_argument('MAF', metavar='arg2', help='minimum MAF', type=float)
 args = parser.parse_args()
 
+#print args.inputFile.readlines()
+
+#print(args.inputFile.read())
+
+for line in args.inputFile:
+	line = line.rstrip("\n")
+	lineList = line.split("\t")
+	lineListSize = len(lineList)
+	print lineListSize
+	#print line
 
 
+args.inputFile.close()
+
+
+def removeLastColumnIfEmpty(myList):
+	listSize = len(myList)
+	for element in myList:
+		
+	return
