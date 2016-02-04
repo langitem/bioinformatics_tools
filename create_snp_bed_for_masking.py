bbc63f38280bb2d1b48f8467d02f102c4a2b3d16
Emanuel Langit <emanuel.langit@fastmail.fm>
2016-02-03 17:00:09 -0800

    Discovered that allele frequency is always in same column

--- create_snp_bed_for_masking.py
+++ create_snp_bed_for_masking.py
@@ -1,59 +1,69 @@
 #!/usr/bin/python
 """
 Author: Emanuel Langit
-
-
+Description: This program takes as input a tab-delimited file downloaded from UCSC
+that contains SNP information (i.e., chromosomal positions, allele frequencies)
+and a minimum minor allele frequency. A bed file is then printed to STDOUT of the
+chromosome, start, stop, and frequencies.
 """
 
 import argparse
+import re
 
 # --- Start Functions ---#
 
-def get_maf(myList):
+def accept_or_reject(myList, mafCutOff):
+
+	# allele frequencies are in the second to the last column:
 	listSize = len(myList)
+	freqColumn = myList[listSize - 2]
+	freqColumn = freqColumn.rstrip(",")
+	freqColumnList = freqColumn.split(',')
 
-	# Use loop and start at last element of the list.
-	# If the element is has no value (""), then keep going back until
-	# it does have value. This is done because the file from UCSC has empty
-	# elements in its tab-delimitted file.
+	largestFreq = 0
+	for frequency in freqColumnList:
 
-	listLength = len(myList)
-	maf = ""
+		# if frequency isn't numeric, exit the loop:
+		if (re.search('[a-zA-Z]', frequency)) or frequency == "":
+			largestFreq = 0;
+			break
+		else:
+			frequency = float(frequency)
 
-	#if myList[listLength-1] == "":
-	#	print 'empty'
-	
-	while maf == "":
-		maf = myList[listLength-1]
-		if maf == "":
-			listLength = listLength-1
+		if frequency > largestFreq:
+			largestFreq = frequency
 
+	answer = ""
 
-	
-	
-	return maf
+	if largestFreq == 0:
+		answer = "reject"
+	else:
+		currentMaf = 1 - largestFreq
+		if currentMaf >= mafCutOff:
+			answer = "accept"
+		else:
+			answer = "reject"
+
+	return answer
 
 # --- End Functions ---#
 
-parser = argparse.ArgumentParser(description='Program to create SNP bed file ')
+parser = argparse.ArgumentParser(description='Program to create SNP bed file based on given minor allele frequency')
 parser.add_argument('inputFile', metavar='arg1', help = 'file downloaded from UCSC with SNP information', type=argparse.FileType('r'))
 parser.add_argument('MAF', metavar='arg2', help='minimum MAF', type=float)
 args = parser.parse_args()
 
-#print args.inputFile.readlines()
-
-#print(args.inputFile.read())
 
 for line in args.inputFile:
 	line = line.rstrip("\n")
 	lineList = line.split("\t")
 
-	maf = get_maf(lineList)
+	freqColIndex = len(lineList) - 2
 
-	print "%s\t%s\t%s\t%s" % (lineList[1], lineList[2], lineList[3], maf)
+	maf = accept_or_reject(lineList, args.MAF)
 
+	if accept_or_reject(lineList, args.MAF) == "accept":
+		print "%s\t%s\t%s\t%s\t%s" % (lineList[1], lineList[2], lineList[3], lineList[4], lineList[freqColIndex].rstrip(","))
 
 
 args.inputFile.close()
-
-
